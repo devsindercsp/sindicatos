51859dcc168e5060dfc16259a19fa12a34b7ce75
devsindercsp <158367686+devsindercsp@users.noreply.github.com>
2024-03-20 10:16:48 -0300

    sindicatos.ts

--- src/data/sindicatos.ts
+++ src/data/sindicatos.ts
@@ -23,7 +23,6 @@ export const sindicatos: Sindicato[] = [
     website: 'www.fetercesp.org.br',
     basedata: 'Junho',
     coverage: [
-      'SETE BARRAS DOIS',
       'CANAS',
       'ESPÍRITO SANTO DO PINHAL',
       'JUQUITIBA',
